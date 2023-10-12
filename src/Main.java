dfcfaf566172b28286e82d3db3447e0292a12d0b
Aaron Lal <lucidlear@gmail.com>
2023-10-12 19:45:32 +1100

    O(n)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public void log(int[] numbers) {
-        System.out.println(numbers[0]);
+
+    public void log(int[] numbers, String[] names) {
+        for (int number : numbers)
+            System.out.println(number);
+
+        for (String name : names)
+            System.out.println(name);
     }
+
 }
