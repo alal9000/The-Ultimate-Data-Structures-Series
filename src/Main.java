ca5e643955680883bf30be61ac4ddfcda67f5774
Aaron Lal <lucidlear@gmail.com>
2023-10-13 18:59:16 +1100

    O(n^2)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {1, 2, 3, 4, 5, 6};
 
         log(numbers);
     }
