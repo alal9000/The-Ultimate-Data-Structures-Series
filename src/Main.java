9e4db6613c839dda002f896384ebb5bce8d8704d
Aaron Lal <lucidlear@gmail.com>
2023-10-19 11:10:54 +1100

    working with linked lists

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.remove(0);
-        list.toArray();
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        System.out.println(list.contains(10));
+        System.out.println(list.indexOf(10));
         System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+
     }
 
 }
