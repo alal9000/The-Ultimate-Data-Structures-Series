e0ed8538eefd7f3f55812216b22e7e6975947db7
Aaron Lal <lucidlear@gmail.com>
2023-10-25 07:47:54 +1100

    converting linked lists to arrays

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        System.out.println(list.size());
-
         list.addFirst(10);
-        System.out.println(list.size());
-
         list.addLast(20);
         list.addLast(30);
-        System.out.println(list.size());
-
-        list.removeLast();
-        list.removeFirst();
-        System.out.println(list.size());
-
-
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 
 
