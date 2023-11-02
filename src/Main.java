fecc49c9bcfb96a422187252dee7dc9d6f7ed5db
Aaron Lal <lucidlear@gmail.com>
2023-11-03 10:02:24 +1100

    solution - reversing a linked list

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.addFirst(10);
-        list.addLast(20);
-        list.addLast(30);
+//        list.addFirst(10);
+//        list.addLast(20);
+//        list.addLast(30);
+        list.reverse();
         var array = list.toArray();
         System.out.println(Arrays.toString(array));
     }
