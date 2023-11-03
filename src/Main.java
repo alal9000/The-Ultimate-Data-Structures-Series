ff594311869551805320eeaf9c2a03fdfe0caa8f
Aaron Lal <lucidlear@gmail.com>
2023-11-03 19:59:46 +1100

    solution - reversing a string

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
         String str = "abcd";
 
-        Stack<Character> stack = new Stack<>();
-        char[] charArray = str.toCharArray();
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse(null);
+        System.out.println(result);
 
-        // Use a for loop to push characters in reverse order
-        for (int i = charArray.length - 1; i >= 0; i--) {
-            stack.push(charArray[i]);
-        }
-
-        System.out.println(stack);
     }
 }
