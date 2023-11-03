981889b5c5b32ed26e7b3386e15fcd44ceacb26c
Aaron Lal <lucidlear@gmail.com>
2023-11-03 14:51:53 +1100

    working with stacks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        System.out.println(list.getKthFromTheEnd(1));
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack);
+        var top = stack.pop();
+        System.out.println(top);
+        System.out.println(stack);
+        top = stack.peek();
+        System.out.println(top);
 
     }
 
