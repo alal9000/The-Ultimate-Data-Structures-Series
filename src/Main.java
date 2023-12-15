1d745370eda43d3ed25bd10c1b59d5c5c18cb5f4
Aaron Lal <lucidlear@gmail.com>
2023-12-16 07:39:14 +1100

    sets

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        CharacterFinder finder = new CharacterFinder();
-        var c = finder.findFirstNonRepeatingChar("a green apple");
-        System.out.println(c);
+        Set<Integer> set = new HashSet<>();
+        int[] numbers = { 1,2 ,3 , 3, 2, 1, 4 };
+        for (var number : numbers)
+            set.add(number);
+        System.out.println(set);
 
     }
 
