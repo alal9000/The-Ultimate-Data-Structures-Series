6723682de6e0db87cde833d25a3657ac6b49900b
Aaron Lal <lucidlear@gmail.com>
2023-12-19 09:48:16 +1100

    hash functions

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        CharacterFinder finder = new CharacterFinder();
-        var ch = finder.findFirstRepeatedChar("green apple");
-        System.out.println(ch);
+        var str = "Orange";
+        System.out.println(str.hashCode());
 
+
+//        Map<String, String> map = new HashMap<>();
+//        map.put("123456-A", "Mosh");
+//        System.out.println(hash("123456-A"));
+
+    }
+
+    public static int hash(String key) {
+        int hash = 0;
+        for (var ch : key.toCharArray())
+            hash += ch;
+        return hash % 100;
     }
 
 }
